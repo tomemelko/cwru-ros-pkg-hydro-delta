226ac5b59b5c8934b27fc9d956f4808343ebd5ee
Theodore Nowak <tsn11@case.edu>
2015-04-30 01:19:20 -0400

    max speed, omega, and omega controller gain for abby

--- catkin/src/cwru_376_student/delta_steering_algorithm/src/delta_steering_algorithm.h
+++ catkin/src/cwru_376_student/delta_steering_algorithm/src/delta_steering_algorithm.h
@@ -34,12 +34,12 @@
 #include <Eigen/LU>
 
 const double UPDATE_RATE = 50.0; // choose the desired-state publication update rate
-const double K_PHI= 1; // control gains for steering (5 is optimized for gazebo; 1 is optimized for jinx)
+const double K_PHI= 2; // control gains for steering (5 is optimized for gazebo; 1 is optimized for jinx)
 const double K_DISP = 3.0;
 const double K_TRIP_DIST = 1.0;
 // dynamic limitations:  these apply to the steering controller; they may be larger than the limits on des state generation
 const double MAX_SPEED = .75; // m/sec; adjust this
-const double MAX_OMEGA = .75; //1.0; // rad/sec; adjust this
+const double MAX_OMEGA = 1.0; //1.0; // rad/sec; adjust this
 
 // variable used for omega controller
 const double d_thresh = 1; //threshold for lateral offset
